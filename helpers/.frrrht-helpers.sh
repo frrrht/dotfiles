fb43a64f8b7d79ccea1c7e352403c3dd3b369041
frrrht <20796116+frrrht@users.noreply.github.com>
2024-08-12 14:30:10 +0200

    fixes

--- helpers/.frrrht-helpers.sh
+++ helpers/.frrrht-helpers.sh
@@ -16,15 +16,16 @@ dev() {
 		echo -e "\n\e[33mGlobal shell helpers:\e[0m"
 		echo -e "  \e[32mreloadshell\e[0m\t\tReload the shell configuration."
 		echo -e "  \e[32meditshell\e[0m\t\tEdit the shell configuration and reload it when closing."
+		echo -e "  \e[32mdotfiles\e[0m\t\tNavigate to the dotfiles directory, pull the latest changes and reload the shell."
 
 		echo -e "\n\e[33mMacOS helpers:\e[0m"
 		echo -e "  \e[32mcat\e[0m\t\t\tConcatenate and print files (on steroids). Plain text, no paging, no line numbers"
 		echo -e "  \e[32mcatt\e[0m\t\t\tConcatenate and print files (on steroids) without paging and with line numbers."
 		echo -e "  \e[32mcattt\e[0m\t\t\tConcatenate and print files (on steroids) with scrollable paging and line numbers."
 		echo -e "  \e[32mvim\e[0m\t\t\tNeoVIM, hyperextensible Vim-based text editor. (hint: ^c + :qa! & ^c + :wq)"
-		echo -e "  \e[32mzipfix\e[0m\t\t\tRemove .DS_Store files for all .zip archives in the current working directory."
+		echo -e "  \e[32mzipfix\e[0m\t\tRemove .DS_Store files for all .zip archives in the current working directory."
 		echo -e "  \e[32mcd\e[0m\t\t\tZioxide's efficient directory browser."
-		echo -e "  \e[32mfinder\e[0m\t\t\tOpen the current working directory in Finder."
+		echo -e "  \e[32mfinder\e[0m\t\tOpen the current working directory in Finder."
 
 		echo -e "\n\e[33mDevelopment helpers:\e[0m"
 		echo -e "  \e[32msubl\e[0m\t\t\tOpen the specified directory or file in Sublime Text."
@@ -40,10 +41,10 @@ dev() {
 		echo -e "  \e[32mpest\e[0m\t\t\tRuns Pest tests in the current context"
 		echo -e "  \e[32mpestf\e[0m\t\t\tRuns Pest tests in the current context with a filter"
 		echo -e "  \e[32mpestp\e[0m\t\t\tRuns Pest tests in the current context in parallel"
-		echo -e "  \e[32mpestpf\e[0m\t\t\tRuns Pest tests in the current context in parallel with a filter"
+		echo -e "  \e[32mpestpf\e[0m\t\tRuns Pest tests in the current context in parallel with a filter"
 		echo -e "  \e[32mpint\e[0m\t\t\tRuns Pint in the current context"
 		echo -e "  \e[32mpintf\e[0m\t\t\tRuns Pint in the current context with a filter"
-		echo -e "  \e[32mduster\e[0m\t\t\tRuns Duster in the current context"
+		echo -e "  \e[32mduster\e[0m\t\tRuns Duster in the current context"
 		echo -e "  \e[32mphpstan\e[0m\t\tRuns PHPStan in the current context"
 		echo -e "  \e[32mmfs\e[0m\t\t\tRuns php artisan migrate:fresh --seed"
 	else
@@ -96,7 +97,7 @@ ar() {
   if [ $# -eq 0 ]; then
     php artisan
   else
-    php artisan "$*"
+    php artisan $*
   fi
 }
 alias sail='vendor/bin/sail'
